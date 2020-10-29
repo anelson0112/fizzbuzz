e7d92c81587785f0c1272b8ba99e6262c1f9cbba
anelson0112 <71732184+anelson0112@users.noreply.github.com>
2020-10-29 10:40:36 -0500

    take 8

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,51 +1,90 @@
 'use strict'
-//tell it to look for the items in the DOM
-document.addEventListener
-//make the squares change color and text when pressed
-function showFizz (element){
-    element.innerText= "Fizz";
-}
-function showBuzz (element){
-    element.innerText= "Buzz";
-}
-function showFizzBuzz (element){
-    element.innerText= "Fizz Buzz";
-}
-function none (element){
-    element.style.backgroundColor='grey';
-}
 
-function fizzMath(squareValue, element){
-//establish for loop to print 1-100
-for (var i= 1; i <=100; i++){
+var totalRows = 10;
+var cellsInRow = 10;
 
-    switch (0){
-        case (i % 15):
-            showFizzBuzz (element);
-            break;
+function drawTable(){
+    //get reference for the grid
+    var grid = document.getElementById(grid);
+
+    var tbl = document.createElement ("table");
+
+    for (var r=0; r < totalRows; r++){
+        var row = document.createElement("tr");
+
+        for (var c= 0; c < cellsInRow; c++) {
+
+            var cell=document.createElement ("td");
+        var cellText = document.createTextNode(buzzMath());
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+        
+    
+
+function buzzMath (){
+    //establish for loop to print 1-100
+        for (var i= 1; i <=100; i++){
+
+            switch (0){
+                case (i % 15):
+                showFizzBuzz (element);
+                    break;
 //look if it is divisable by 15 if yes log fizzbuzz if not move next       
-        case (i % 3):
-            showFizz (element);
-            break;
+                case (i % 3):
+                showFizz (element);
+                break;
 //look if divisable by 3 if yes log fizz if not move next            
-        case (i % 5):
-            showBuzz (element);
-            break
+                case (i % 5):
+                showBuzz (element);
+                    break
 //look if divisable by 5 if yes log buzz if not move next            
-        default:
-            none(element);
+                default:
+                notAFB(element);
 //if none of the previous statements are true log i from the loop
+        
+    
+            }}
+        }   
+    
     }
-}}
-//create var for the numbers
+    
+}  
+    
+    
+    
+    
+    
+    //tell it to look for the items in the DOM
+/*document.addEventListener('DOMContentLoaded',function(){
+    
 
-var numbers=document.querySelectorAll('.number');
 
-//event listners
-for (var i=0; i < numbers.length; i++){
-    var numClick= numbers[i].addEventListener ('click', function() {
-        var squareValue = parseInt(this.innerText);
-        var element=this;
-        fizzMath (squareValue,element);
-    });
-    }
+    //make the squares change color and text when pressed
+          function showFizz (element){
+      element.innerText= "Fizz";
+          }
+          function showBuzz (element){
+              element.innerText= "Buzz";
+          }
+          function showFizzBuzz (element){
+              element.innerText= "Fizz Buzz";
+          }
+          function notAFB (element){
+              element.innerText= "Not";
+          }
+  
+      function fizzMath(squareValue, element){
+    
+    
+            var numbers=document.querySelectorAll('.number');
+
+    for (var i=0; i < numbers.length; i++){
+        var numClick = numbers[i].addEventListener('click',function(){
+        var squareValue=parseInt(element.innerText);
+        
+        fizzMath(squareValue,element);
+        
+        });
+
+    
+} */
