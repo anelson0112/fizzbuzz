8387027efc226a776e688e4eca379a83c6ca1967
anelson0112 <71732184+anelson0112@users.noreply.github.com>
2020-10-30 08:54:38 -0500

    work please

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,6 @@
 'use strict'
 //define start button
-document.getElementById("start").onClick=buzzMath;
+//document.getElementById("start").onClick;
 
 //to put result in number box
 //var element=document.getElementsByClassName("number").innerText
@@ -8,32 +8,33 @@ document.getElementById("start").onClick=buzzMath;
 
 //the math
 
- function fizzMath(){   
-var display = document.getElementById('display');       
-var result= '';
-var displayHTML = "";
+   
+let disp = document.getElementById("display");       
+
+
 
     //establish for loop to print 1-100
         for (var i= 1; i <=100; i++){
 
             switch (0){
                 case (i % 15):
-                result="Fizz Buzz";
+                disp.innerHTML +="Fizz Buzz";
+                
                     break;
 //look if it is divisable by 15 if yes log fizzbuzz if not move next       
                 case (i % 3):
-                    result="Fizz";
+                    disp.innerHTML +="Fizz";
                 break;
 //look if divisable by 3 if yes log fizz if not move next            
                 case (i % 5):
-                    result="Buzz";
+                    disp.innerHTML +="Buzz";
                     break
 //look if divisable by 5 if yes log buzz if not move next            
                 default:
-                    result=(i);
+                    disp.innerHTML +=(i);
 //if none of the previous statements are true log i from the loop
         
-    displayHTML += "<p>" + result + "<p>";            }
+   
             
         }
     
@@ -85,4 +86,4 @@ var numClick = number[i].addEventListener('click',function(){
 } */
  
 
-//function for each result of loop
+/*function for each result of loop*/
